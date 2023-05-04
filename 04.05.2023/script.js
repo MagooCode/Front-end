c717977133a689ce2717530aa2e2dde1c3b293b0
MagooCode <125905039+MagooCode@users.noreply.github.com>
2023-05-04 17:12:26 +0400

    Add files via upload

---
+++ 04.05.2023/script.js
@@ -0,0 +1,45 @@
+// function returnDuplicate(numbers) {
+//     let numbersChecker = [];
+
+//     for (let number of numbers) {
+//         numbersChecker[number] = numbersChecker[number] ?  numbersChecker[number] + 1 : 1;
+//         if (numbersChecker[number] > 1) {
+//             return number;
+//         }
+//     }
+
+//     return null;
+// }
+// let numbers = [1, 2 , 3, 4, 4, 5, 6, 7];
+// console.log(returnDuplicate(numbers));
+
+//===================================================================================
+
+// function returnHighestValue(numbers) {
+//     let HighestValue = 0;
+//     for (let index = 0; index < numbers.length; index++) {
+//         if (HighestValue < numbers[index])
+//         {
+//             HighestValue = numbers[index];
+//         }
+//     }
+//     return HighestValue;
+// }
+
+// let numbers = [10, 35, 50, 20];
+// console.log(returnHighestValue(numbers));
+
+//===================================================================================
+
+// function arrayX2(numbers) {
+//     return numbers.map(number => number * 2);
+// }
+
+// let numbers = [10,20,30,40,50];
+// console.log(arrayX2(numbers));
+
+//===================================================================================
+
+
+
+

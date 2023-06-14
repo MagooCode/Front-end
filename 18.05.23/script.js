00b5be2d28e58f5c188aac8af03484f123b14166
MagooCode <125905039+MagooCode@users.noreply.github.com>
2023-06-14 18:18:32 +0400

    Add files via upload

---
+++ 18.05.23/script.js
@@ -0,0 +1,14 @@
+let username='MagooCode';
+
+fetch(`https://api.github.com/users/${username}`)
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById('profile-img').src = data.avatar_url;
+        document.getElementById('name').textContent = data.name;
+        document.getElementById('username').textContent = `@${data.login}`;
+        document.getElementById('location').innerHTML = `<i class="fa-solid fa-location-dot"></i> ${data.location}`;
+        document.getElementById('followers').textContent = data.followers;
+        document.getElementById('follows').textContent = data.following;
+        document.getElementById('github-profile').href = data.html_url;
+    })
+    .catch(error => console.log(error));

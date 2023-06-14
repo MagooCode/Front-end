25d675f3f6cf1004e41ae852806527e6d7bdaa74
MagooCode <125905039+MagooCode@users.noreply.github.com>
2023-06-14 18:02:13 +0400

    Add files via upload

---
+++ 17.05.23 fetch-api/script.js
@@ -0,0 +1,45 @@
+document.addEventListener("DOMContentLoaded", getData);
+
+async function getData() {
+  try {
+    const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await response.json();
+    ShowData(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+function ShowData(data) {
+    const users = {};
+  const userList = document.getElementById("userList");
+
+
+  data.forEach(item => {
+    const { userId, title } = item;
+    users[userId] = users[userId] || [];
+    users[userId].push(title);
+  });
+
+  for (const userId in users) 
+  {
+
+
+    const tasks = users[userId];
+    const userItem = document.createElement("li");
+    const userHeading = document.createElement("h2");
+    userHeading.textContent = "User " + userId;
+    userItem.appendChild(userHeading);
+
+    const taskList = document.createElement("ul");
+    tasks.forEach(title => 
+        {
+      const taskItem = document.createElement("li");
+      taskItem.textContent = title;
+      taskList.appendChild(taskItem);
+    });
+
+    userItem.appendChild(taskList);
+    userList.appendChild(userItem);
+  }
+}
